dd061c12e6d0dfcf2d54aaaa21ebf834dda8f0ee
AgusNeira <a2002neira@gmail.com>
2021-01-04 21:52:10 -0300

    [ADD] Updated check functionality

--- prime-finder.c
+++ prime-finder.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int isPrime(long number) {
 	long root = sqrt(number);
@@ -12,22 +13,29 @@ int isPrime(long number) {
 }
 
 int main(int argc, char **argv) {
-	long temp_num;
-	char *end;
-	for (int i = 1; i < argc; i++) {
-		temp_num = strtol(argv[i], &end, 10);
+	if (argc < 2) {
+		printf("Error: sub-command not specified\n");
+		return 1;
+	}
 
-		if (isPrime(temp_num)) {
-			printf("%d IS a prime number\n", temp_num);
-		} else {
-			printf("%d IS NOT a prime number\n", temp_num);
+	if (strcmp(argv[1], "check") == 0) {		// CHECK sub-command
+		long temp_num;
+		char *end;
+
+		if (argc == 2) {
+			printf("Error: no number was given\n");
+			return 1;
 		}
-	}
 
-	if (argc == 1) {
-		printf("Usage:\n");
-		printf("\tprime-finder <number1> [<number2> <number3>...]\n");
-	}
+		for (int i = 2; i < argc; i++) {
+			temp_num = strtol(argv[i], &end, 10);
 
+			if (isPrime(temp_num)) {
+				printf("%d ", temp_num);
+			}
+		}
+		printf("\n");
+	}
+	
 	return 0;
 }
