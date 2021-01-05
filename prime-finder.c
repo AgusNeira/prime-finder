a3d9d8061fcd9d5a11194576aff3f9ec444f55c2
AgusNeira <a2002neira@gmail.com>
2021-01-05 02:15:35 -0300

    Integrated feature with main program

--- prime-finder.c
+++ prime-finder.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "check.h"
+#include "interval.h"
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
@@ -10,8 +11,10 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	if (strcmp(argv[1], "check") == 0) {		// CHECK sub-command
+	if (strcmp(argv[1], "check") == 0) {
 		return check(argc, argv);
+	} else if (strcmp(argv[1], "interval") == 0) {
+		return interval(argc, argv);
 	}
 	
 	return 0;
